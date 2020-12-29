367a8165addf6a5a6e950aa82ec144bb1f2768d5
LuckyNoS7evin <andy.ramit@gmail.com>
2020-12-29 14:38:19 +0000

    #8 Added Relatives Calculations

---
+++ src/irsdkSharp.Calculation/Models/CarRelativeModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace irsdkSharp.Calculation.Models
+{
+    public class CarRelativeModel
+    {
+        public TimeSpan Ahead { get; set; }
+        public TimeSpan Behind { get; set; }
+    }
+}
