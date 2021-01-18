c2b776cfe2effd63f51e040261438f09ea557d40
LuckyNoS7evin <andy.ramit@gmail.com>
2021-01-18 13:16:57 +0000

    reducing CPU load 1) Session data updates were not recognised 2) Get Data should only be called "when you need it" 3) Need to test (everything) 4) need to speed up serialization

--- src/irsdkSharp.Serialization/IRacingSDKExtensions.cs
+++ src/irsdkSharp.Serialization/IRacingSDKExtensions.cs
@@ -23,44 +23,19 @@ namespace irsdkSharp.Serialization
 
         public static IRacingDataModel GetSerializedData(this IRacingSDK racingSDK)
         {
-            if (racingSDK.IsInitialized)
+            if (racingSDK.IsInitialized && racingSDK.Header != null)
             {
                 var length = (int)IRacingSDK.GetFileMapView(racingSDK).Capacity;
                 var data = new byte[length];
                 IRacingSDK.GetFileMapView(racingSDK).ReadArray(0, data, 0, length);
-
-                //Get header
-                var header = new IRacingSdkHeader(data);
-                var headers = GetVarHeaders(racingSDK,header, data);
-
                 //Serialise the string into objects, tada!
-                return IRacingDataModel.Serialize(data[header.Buffer..(header.Buffer + header.BufferLength)], headers);
+                return IRacingDataModel.Serialize(
+                    data[racingSDK.Header.Buffer..(racingSDK.Header.Buffer + racingSDK.Header.BufferLength)],
+                    racingSDK.VarHeaders);
             }
             return null;
         }
 
-        private static List<VarHeader> GetVarHeaders(IRacingSDK racingSDK, IRacingSdkHeader header, Span<byte> span)
-        {
-            var nullChar = new char[] { '\0' };
-            var headers = new List<VarHeader>();
-            for (int i = 0; i < header.VarCount; i++)
-            {
-                int type = BitConverter.ToInt32(span.Slice(header.VarHeaderOffset + (i * VarHeader.Size)));
-                int offset = BitConverter.ToInt32(span.Slice(header.VarHeaderOffset + (i * VarHeader.Size) + IRacingSDK.VarOffsetOffset));
-                int count = BitConverter.ToInt32(span.Slice(header.VarHeaderOffset + (i * VarHeader.Size) + IRacingSDK.VarCountOffset));
-                string nameStr = IRacingSDK.GetEncoding(racingSDK)
-                    .GetString(span.Slice(header.VarHeaderOffset + (i * VarHeader.Size) + IRacingSDK.VarNameOffset, Constants.MaxString))
-                    .TrimEnd(nullChar);
-                string descStr = IRacingSDK.GetEncoding(racingSDK)
-                    .GetString(span.Slice(header.VarHeaderOffset + (i * VarHeader.Size) + IRacingSDK.VarDescOffset, Constants.MaxDesc))
-                    .TrimEnd(nullChar);
-                string unitStr = IRacingSDK.GetEncoding(racingSDK)
-                    .GetString(span.Slice(header.VarHeaderOffset + (i * VarHeader.Size) + IRacingSDK.VarUnitOffset, Constants.MaxString))
-                    .TrimEnd(nullChar);
-                headers.Add(new VarHeader(type, offset, count, nameStr, descStr, unitStr));
-            }
-            return headers;
-        }
     }
 
 }
