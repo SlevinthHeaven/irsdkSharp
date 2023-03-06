e1fe0bd3cd3d35cb9ed0a5bd2b6df3322b662ec2
simo026q <simonchristensen03@gmail.com>
2023-03-06 17:35:37 +0100

    Added Options class for later use

---
+++ src/irsdkSharp/IRacingSdkDefaults.cs
@@ -0,0 +1,22 @@
+﻿namespace irsdkSharp;
+
+/// <summary>
+/// The default values for the iRacing SDK
+/// </summary>
+public static class IRacingSdkDefaults
+{
+    /// <summary>
+    /// The default updates per second.
+    /// </summary>
+    public const int DefaultUpdateFrequency = 1;
+    
+    /// <summary>
+    /// The maximum updates per second supported by iRacing.
+    /// </summary>
+    public const int MaxUpdateFrequency = 60;
+    
+    /// <summary>
+    /// The default delay between a connection check when the sim is not connected.
+    /// </summary>
+    public const int DefaultCheckConnectionDelay = 5000;
+}
