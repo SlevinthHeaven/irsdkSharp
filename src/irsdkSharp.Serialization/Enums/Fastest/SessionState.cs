f56d34bbc67d8bcae470598ce14eb46d90cee556
Marvin Herbold <marvin.herbold@gmail.com>
2023-05-29 07:40:46 -0700

    Session state enum

---
+++ src/irsdkSharp.Serialization/Enums/Fastest/SessionState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace irsdkSharp.Serialization.Enums.Fastest
+{
+	public enum SessionState
+	{
+		StateInvalid,
+		StateGetInCar,
+		StateWarmup,
+		StateParadeLaps,
+		StateRacing,
+		StateCheckered,
+		StateCoolDown
+	}
+}
