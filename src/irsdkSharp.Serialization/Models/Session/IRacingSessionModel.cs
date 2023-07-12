2175563ed7fbb324078de4b424aeea5d967bcee4
Marvin Herbold <marvin.herbold@gmail.com>
2023-07-12 07:35:22 -0700

    Added code to fix invalid iRacing YAML issues.

--- src/irsdkSharp.Serialization/Models/Session/IRacingSessionModel.cs
+++ src/irsdkSharp.Serialization/Models/Session/IRacingSessionModel.cs
@@ -7,6 +7,9 @@ using irsdkSharp.Serialization.Models.Session.SplitTimeInfo;
 using irsdkSharp.Serialization.Models.Session.WeekendInfo;
 using System;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 using YamlDotNet.Serialization;
 
 namespace irsdkSharp.Serialization.Models.Session
@@ -15,6 +18,7 @@ namespace irsdkSharp.Serialization.Models.Session
     {
         public static IRacingSessionModel Serialize(string yaml)
         {
+            yaml = PreprocessYAML(yaml);
             var r = new StringReader(yaml);
             var deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
             try
@@ -36,5 +40,92 @@ namespace irsdkSharp.Serialization.Models.Session
         public RadioInfoModel RadioInfo { get; set; }
         public DriverInfoModel DriverInfo { get; set; }
         public SplitTimeInfoModel SplitTimeInfo { get; set; }
+
+        public class KeyTracker
+        {
+            public int counter = 0;
+            public bool ignoreUntilNextLine = false;
+            public bool addFirstQuote = false;
+            public bool addSecondQuote = false;
+        }
+
+        private static string PreprocessYAML( string yaml )
+        {
+            const int MaxNumDrivers = 63;
+            const int MaxNumAdditionalCharactersPerFixedKey = 2;
+
+            var keysToFix = new string[]
+            {
+                "AbbrevName:", "TeamName:", "UserName:", "Initials:", "DriverSetupName:"
+            };
+
+            var keyTracker = new KeyTracker[ keysToFix.Length ];
+
+            for ( var i = 0; i < keyTracker.Length; i++ )
+            {
+                keyTracker[ i ] = new KeyTracker();
+            }
+
+            var stringBuilder = new StringBuilder( yaml.Length + keysToFix.Length * MaxNumAdditionalCharactersPerFixedKey * MaxNumDrivers );
+
+            foreach ( var c in yaml )
+            {
+                for ( var i = 0; i < keysToFix.Length; i++ )
+                {
+                    if ( keyTracker[ i ].ignoreUntilNextLine )
+                    {
+                        if ( c == '\n' )
+                        {
+                            keyTracker[ i ].counter = 0;
+                            keyTracker[ i ].ignoreUntilNextLine = false;
+                        }
+                    }
+                    else if ( keyTracker[ i ].addFirstQuote )
+                    {
+                        if ( c != ' ' )
+                        {
+                            stringBuilder.Append( '\'' );
+
+                            keyTracker[ i ].addFirstQuote = false;
+                            keyTracker[ i ].addSecondQuote = true;
+                        }
+                    }
+                    else if ( keyTracker[ i ].addSecondQuote )
+                    {
+                        if ( c == '\n' )
+                        {
+                            stringBuilder.Append( '\'' );
+
+                            keyTracker[ i ].counter = 0;
+                            keyTracker[ i ].addSecondQuote = false;
+                        }
+                        else if ( c == '\'' )
+                        {
+                            stringBuilder.Append( '\'' );
+                        }
+                    }
+                    else
+                    {
+                        if ( c == keysToFix[ i ][ keyTracker[ i ].counter ] )
+                        {
+                            keyTracker[ i ].counter++;
+
+                            if ( keyTracker[ i ].counter == keysToFix[ i ].Length )
+                            {
+                                keyTracker[ i ].addFirstQuote = true;
+                            }
+                        }
+                        else if ( c != ' ' )
+                        {
+                            keyTracker[ i ].ignoreUntilNextLine = true;
+                        }
+                    }
+                }
+
+				stringBuilder.Append( c );
+			}
+
+			return stringBuilder.ToString();
+        }
     }
 }
