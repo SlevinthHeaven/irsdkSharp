1cf8e358876229d8afeecb81061fc6973481f5ad
Marvin Herbold <marvin.herbold@gmail.com>
2023-07-12 07:50:19 -0700

    Cleaner version of the code.

--- src/irsdkSharp.Serialization/Models/Session/IRacingSessionModel.cs
+++ src/irsdkSharp.Serialization/Models/Session/IRacingSessionModel.cs
@@ -14,118 +14,122 @@ using YamlDotNet.Serialization;
 
 namespace irsdkSharp.Serialization.Models.Session
 {
-    public class IRacingSessionModel
-    {
-        public static IRacingSessionModel Serialize(string yaml)
-        {
-            yaml = PreprocessYAML(yaml);
-            var r = new StringReader(yaml);
-            var deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
-            try
-            {
-                return deserializer.Deserialize<IRacingSessionModel>(r);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-                return null;
-            }
-        }
+	public class IRacingSessionModel
+	{
+		public static IRacingSessionModel Serialize( string yaml )
+		{
+			yaml = PreprocessYAML( yaml );
+			var r = new StringReader( yaml );
+			var deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
+			try
+			{
+				return deserializer.Deserialize<IRacingSessionModel>( r );
+			}
+			catch ( Exception ex )
+			{
+				Console.WriteLine( ex.Message );
+				Console.WriteLine( ex.StackTrace );
+				return null;
+			}
+		}
 
-        public WeekendInfoModel WeekendInfo { get; set; }
-        public SessionInfoModel SessionInfo { get; set; }
-        // public QualifyResultsInfoModel QualifyResultsInfo { get; set; }
-        public CameraInfoModel CameraInfo { get; set; }
-        public RadioInfoModel RadioInfo { get; set; }
-        public DriverInfoModel DriverInfo { get; set; }
-        public SplitTimeInfoModel SplitTimeInfo { get; set; }
+		public WeekendInfoModel WeekendInfo { get; set; }
+		public SessionInfoModel SessionInfo { get; set; }
+		// public QualifyResultsInfoModel QualifyResultsInfo { get; set; }
+		public CameraInfoModel CameraInfo { get; set; }
+		public RadioInfoModel RadioInfo { get; set; }
+		public DriverInfoModel DriverInfo { get; set; }
+		public SplitTimeInfoModel SplitTimeInfo { get; set; }
 
-        public class KeyTracker
-        {
-            public int counter = 0;
-            public bool ignoreUntilNextLine = false;
-            public bool addFirstQuote = false;
-            public bool addSecondQuote = false;
-        }
+		public class KeyTracker
+		{
+			public string keyToFix = string.Empty;
+			public int counter = 0;
+			public bool ignoreUntilNextLine = false;
+			public bool addFirstQuote = false;
+			public bool addSecondQuote = false;
+		}
 
-        private static string PreprocessYAML( string yaml )
-        {
-            const int MaxNumDrivers = 63;
-            const int MaxNumAdditionalCharactersPerFixedKey = 2;
+		private static string PreprocessYAML( string yaml )
+		{
+			const int MaxNumDrivers = 63;
+			const int MaxNumAdditionalCharactersPerFixedKey = 2;
 
-            var keysToFix = new string[]
-            {
-                "AbbrevName:", "TeamName:", "UserName:", "Initials:", "DriverSetupName:"
-            };
+			var keysToFix = new string[]
+			{
+				"AbbrevName:", "TeamName:", "UserName:", "Initials:", "DriverSetupName:"
+			};
 
-            var keyTracker = new KeyTracker[ keysToFix.Length ];
+			var keyTrackers = new KeyTracker[ keysToFix.Length ];
 
-            for ( var i = 0; i < keyTracker.Length; i++ )
-            {
-                keyTracker[ i ] = new KeyTracker();
-            }
+			for ( var i = 0; i < keyTrackers.Length; i++ )
+			{
+				keyTrackers[ i ] = new KeyTracker()
+				{
+					keyToFix = keysToFix[ i ]
+				};
+			}
 
-            var stringBuilder = new StringBuilder( yaml.Length + keysToFix.Length * MaxNumAdditionalCharactersPerFixedKey * MaxNumDrivers );
+			var stringBuilder = new StringBuilder( yaml.Length + keysToFix.Length * MaxNumAdditionalCharactersPerFixedKey * MaxNumDrivers );
 
-            foreach ( var c in yaml )
-            {
-                for ( var i = 0; i < keysToFix.Length; i++ )
-                {
-                    if ( keyTracker[ i ].ignoreUntilNextLine )
-                    {
-                        if ( c == '\n' )
-                        {
-                            keyTracker[ i ].counter = 0;
-                            keyTracker[ i ].ignoreUntilNextLine = false;
-                        }
-                    }
-                    else if ( keyTracker[ i ].addFirstQuote )
-                    {
-                        if ( c != ' ' )
-                        {
-                            stringBuilder.Append( '\'' );
+			foreach ( var c in yaml )
+			{
+				foreach ( var keyTracker in keyTrackers )
+				{
+					if ( keyTracker.ignoreUntilNextLine )
+					{
+						if ( c == '\n' )
+						{
+							keyTracker.counter = 0;
+							keyTracker.ignoreUntilNextLine = false;
+						}
+					}
+					else if ( keyTracker.addFirstQuote )
+					{
+						if ( c != ' ' )
+						{
+							stringBuilder.Append( '\'' );
 
-                            keyTracker[ i ].addFirstQuote = false;
-                            keyTracker[ i ].addSecondQuote = true;
-                        }
-                    }
-                    else if ( keyTracker[ i ].addSecondQuote )
-                    {
-                        if ( c == '\n' )
-                        {
-                            stringBuilder.Append( '\'' );
+							keyTracker.addFirstQuote = false;
+							keyTracker.addSecondQuote = true;
+						}
+					}
+					else if ( keyTracker.addSecondQuote )
+					{
+						if ( c == '\n' )
+						{
+							stringBuilder.Append( '\'' );
 
-                            keyTracker[ i ].counter = 0;
-                            keyTracker[ i ].addSecondQuote = false;
-                        }
-                        else if ( c == '\'' )
-                        {
-                            stringBuilder.Append( '\'' );
-                        }
-                    }
-                    else
-                    {
-                        if ( c == keysToFix[ i ][ keyTracker[ i ].counter ] )
-                        {
-                            keyTracker[ i ].counter++;
+							keyTracker.counter = 0;
+							keyTracker.addSecondQuote = false;
+						}
+						else if ( c == '\'' )
+						{
+							stringBuilder.Append( '\'' );
+						}
+					}
+					else
+					{
+						if ( c == keyTracker.keyToFix[ keyTracker.counter ] )
+						{
+							keyTracker.counter++;
 
-                            if ( keyTracker[ i ].counter == keysToFix[ i ].Length )
-                            {
-                                keyTracker[ i ].addFirstQuote = true;
-                            }
-                        }
-                        else if ( c != ' ' )
-                        {
-                            keyTracker[ i ].ignoreUntilNextLine = true;
-                        }
-                    }
-                }
+							if ( keyTracker.counter == keyTracker.keyToFix.Length )
+							{
+								keyTracker.addFirstQuote = true;
+							}
+						}
+						else if ( c != ' ' )
+						{
+							keyTracker.ignoreUntilNextLine = true;
+						}
+					}
+				}
 
 				stringBuilder.Append( c );
 			}
 
 			return stringBuilder.ToString();
-        }
-    }
+		}
+	}
 }
